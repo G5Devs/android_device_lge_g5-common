b2f148eeb377c1c5efbac649296523391ae85f32
Louis Popi <theh2o64@gmail.com>
2016-05-07 18:22:54 +0200

    g5-common: Downgrade to LA.HB.1.1.1-02740-8x96.0

--- gps/loc_api/libloc_api_50001/Android.mk
+++ gps/loc_api/libloc_api_50001/Android.mk
@@ -41,7 +41,7 @@ LOCAL_CFLAGS += \
 LOCAL_C_INCLUDES:= \
     $(TARGET_OUT_HEADERS)/gps.utils \
     $(TARGET_OUT_HEADERS)/libloc_core \
-    device/lge/g5-common/loc_api/libloc_api_50001 \
+    hardware/qcom/gps/loc_api/libloc_api_50001 \
     $(TARGET_OUT_HEADERS)/libflp
 
 LOCAL_COPY_HEADERS_TO:= libloc_eng/
